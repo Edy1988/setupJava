f4d24729237e04e815f4dbcc89a8a893c7dd6b81
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-02-10 09:38:45 +0000

    Passed test.

--- test/PetRock.java
+++ test/PetRock.java
@@ -1,2 +1,16 @@
+
 public class PetRock {
+
+    private String name;
+
+    public PetRock(String name) {
+
+        this.name = name;
+
+    }
+
+    public String getName() {
+
+        return name ;
+    }
 }
